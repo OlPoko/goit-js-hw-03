65dd23a4c78e5aa9d00e39736c379bd90c33a5b5
OlPoko <s.pokotilo@gmail.com>
2024-11-11 21:45:24 +0200

    add task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,13 @@
-function getSlice(array, value) {
-   const indexArray = array.indexOf(value);
-   if (indexArray >= 0) {
-      return array.slice(0, indexArray + 1)
+function filterArray(numbers, value) {
+   let numberLitl = [];
+   for (let i = 0; i < numbers.length; i++) {
+      if ( numbers[i] > value) { numberLitl.push(numbers[i]) }
    }
-    else {
-    return array.slice(0, 0 )
-    }
-  }
+   return numberLitl; 
+}
 
-  console.log(getSlice(["Mango", "Poly", "Ajax"], "Poly"))
-
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
